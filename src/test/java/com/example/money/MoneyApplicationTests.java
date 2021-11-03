3acc1aa6214a85ed4e8624515ac845ffde4c3f11
boscaiolo <boscaiolo.gp@gmail.com>
2021-11-03 18:44:10 +0000

    Added tests for map load

--- src/test/java/com/example/money/MoneyApplicationTests.java
+++ src/test/java/com/example/money/MoneyApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.money;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class MoneyApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void whenMapLoadedGetRatesForDateReturnsCorrectResponse() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates?date=2021-09-28")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void whenMapLoadedGetRatesForDateWithoutDstReturnsCurrentDateRates() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
 
 }
