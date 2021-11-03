7cedb8eedde95c65a9964a8ea2e0b83d891189df
boscaiolo <boscaiolo.gp@gmail.com>
2021-11-03 19:33:50 +0000

    Added integration tests

--- src/test/java/com/example/money/MoneyApplicationTests.java
+++ src/test/java/com/example/money/MoneyApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,12 +34,75 @@ class MoneyApplicationTests {
 	}
 
 	@Test
-	public void whenMapLoadedGetRatesForDateWithoutDstReturnsCurrentDateRates() throws Exception {
+	public void whenMapLoadedGetRatesForDateWithoutDateReturnsCurrentDateRates() throws Exception {
 
 		ResultActions result = mvc.perform(get("/currency-rates")
 						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 	}
 
+	@Test
+	public void whenMapLoadedGetRatesForDateReturnRatesForDate() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates?date=1999-01-04")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void whenMapLoadedGetRatesForDateNotExistingReturnNotFound() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates?date=4000-01-04")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void whenMapLoadedConvertCurrencyForDateReturnsProper() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates/convert-for-date?date=2021-09-28&source=EUR&target=BGN&amount=1")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void whenMapLoadedConvertCurrencyForDateReturnsNotFound() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates/convert-for-date?date=4000-09-28&source=EUR&target=BGN&amount=1")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void whenMapLoadedHighestCurrencyReturnsNotFound() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates/highest-currency-rate-for-period?currency=STO")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void whenMapLoadedHighestCurrencyReturnsProper() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates/highest-currency-rate-for-period?currency=BGN")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void whenMapLoadedAverageCurrencyReturnsNotFound() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates/average-currency-rate-for-period?currency=STO")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void whenMapLoadedAverageCurrencyReturnsProper() throws Exception {
+
+		ResultActions result = mvc.perform(get("/currency-rates/average-currency-rate-for-period?currency=BGN")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 
 }
