0de8419eb28efffd676bb89e8cba63cbd5c6aefe
boscaiolo <boscaiolo.gp@gmail.com>
2021-11-03 16:20:29 +0000

    Added logic to download and extract the .zip file

--- src/main/java/com/example/money/CsvHistoricRatesProvider.java
+++ src/main/java/com/example/money/CsvHistoricRatesProvider.java
@@ -3,47 +3,53 @@ package com.example.money;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+import java.util.zip.ZipFile;
 
 @Component
 public class CsvHistoricRatesProvider implements HistoricRatesProvider {
 
-    final Map<LocalDate, Map<String, BigDecimal>> historicRates = new ConcurrentHashMap<>();
+    private final Map<LocalDate, Map<String, BigDecimal>> historicRates = new ConcurrentHashMap<>();
 
     @PostConstruct
     public void init() {
-        CsvMapper csvMapper = new CsvMapper();
-        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+        ZipFile zipFile = downloadZipFile();
+        if (zipFile != null) {
+            CsvMapper csvMapper = new CsvMapper();
+            CsvSchema schema = CsvSchema.emptySchema().withHeader();
 
-        try (InputStream csvFile = new ClassPathResource("eurofxref-hist.csv").getInputStream()) {
-            MappingIterator<Map<String, String>> mi = csvMapper.readerFor(Map.class)
-                    .with(schema).readValues(csvFile);
-            while (mi.hasNext()) {
-                Map<String, String> rowAsMap = mi.next();
-                Map<String, BigDecimal> dateRates = new ConcurrentHashMap<>();
-                LocalDate date = LocalDate.parse(rowAsMap.get("Date"));
-                rowAsMap.remove("Date");
-                for (Map.Entry<String, String> rate : rowAsMap.entrySet()) {
-                    if (!rate.getValue().equals("N/A")) {
-                        dateRates.put(rate.getKey(), new BigDecimal(rate.getValue()));
+            try (InputStream csvFile = zipFile.getInputStream(zipFile.entries().nextElement())) {
+                MappingIterator<Map<String, String>> mi = csvMapper.readerFor(Map.class)
+                        .with(schema).readValues(csvFile);
+                while (mi.hasNext()) {
+                    Map<String, String> rowAsMap = mi.next();
+                    Map<String, BigDecimal> dateRates = new ConcurrentHashMap<>();
+                    LocalDate date = LocalDate.parse(rowAsMap.get("Date"));
+                    rowAsMap.remove("Date");
+                    for (Map.Entry<String, String> rate : rowAsMap.entrySet()) {
+                        if (!rate.getValue().equals("N/A")) {
+                            dateRates.put(rate.getKey(), new BigDecimal(rate.getValue()));
+                        }
                     }
+                    historicRates.put(date, dateRates);
                 }
-                historicRates.put(date, dateRates);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -71,5 +77,20 @@ public class CsvHistoricRatesProvider implements HistoricRatesProvider {
         }
 
         return currencyRates;
+    }
+
+    private ZipFile downloadZipFile(){
+        try (BufferedInputStream in = new BufferedInputStream(new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip").openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("eurofxref-hist.zip")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+            return new ZipFile("eurofxref-hist.zip");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
